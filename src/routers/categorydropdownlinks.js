c73471a486ad37c6badf1ec7642afe2a18424d6f
Joby-01 <Johan@PROBOOK>
2025-05-21 16:47:55 +0200

    x

--- src/routers/categorydropdownlinks.js
+++ src/routers/categorydropdownlinks.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router';
+
 import {Categoriesdatav2} from "../services/data"
 
 export default function Categorydropdownlinks(props) {
